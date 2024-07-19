4e3397aadee44148f342f38f8b6ada586939dbb1
DileepKumarMantena <deelipkumar261997@gmail.com>
2024-07-19 19:56:59 +0530

    Modified code with comments

--- Task1/captcha.py
+++ Task1/captcha.py
@@ -1,9 +1,18 @@
-import random
+import random  # Import the random module for generating random characters
 
 def generate_captcha_text(length=5):
-    characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    return ''.join(random.choice(characters) for _ in range(length))
+    """
+    Function to generate a random CAPTCHA text of a given length.
+    
+    Args:
+        length (int): Length of the CAPTCHA text to be generated. Default is 5.
+    
+    Returns:
+        str: A string containing the random CAPTCHA text.
+    """
+    characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'  # Define possible characters
+    return ''.join(random.choices(characters, k=length))  # Use random.choices to pick 'length' characters and join them
 
 # Generate Captcha text
-captcha_text = generate_captcha_text()
-print("Generated Captcha Text:", captcha_text)
+captcha_text = generate_captcha_text()  # Call the function to generate CAPTCHA text
+print("Generated Captcha Text:", captcha_text)  # Print the generated CAPTCHA text
